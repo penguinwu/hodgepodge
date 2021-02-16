6d44feebe290dcedb7d501b422a9db1c7db79b71
Peng Wu <pengwu@fb.com>
2021-02-16 08:01:58 -0800

    Add more stats Summary:
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- PerfStudy/scripts/process_ir_graph.py
+++ PerfStudy/scripts/process_ir_graph.py
@@ -6,10 +6,12 @@ from collections import OrderedDict
 from operator import itemgetter
 
 benchmarks = ['alexnet', 'BERT_pytorch', 'densenet121', 'fastNLP', 'LearningToPaint', 'mnasnet1_0', 'mobilenet_v2',
-              'mobilenet_v3', 'pytorch_stargan','pytorch_struct', 'resnet18', 'resnet50',
+              'pytorch_mobilenet_v3', 'pytorch_stargan','pytorch_struct', 'resnet18', 'resnet50',
               'resnext50_32x4d', 'shufflenet_v2_x1_0', 'squeezenet1_1', 'vgg16']
+internal_benchmarks = ['adindexer-merge-net-ctr-mobilefeed', 'deep-and-wide']
 #benchmarks = ['alexnet']
-ops_pattern = ['*', 'aten::', "prim::", "block*()", "prim::CallMethod", "prim::Loop", "prim::SetAttr", "prim::GetAttr"]
+ops_pattern = ['*', 'aten::', "prim::", "fb::", "quantized::", "block*()", "prim::If", "prim::Loop", "prim::CallMethod", "prim::SetAttr"]
+# ops_pattern = ['*', 'aten::', "prim::", "block*()", "prim::CallMethod", "prim::If", "prim::Loop", "prim::SetAttr", "prim::GetAttr"]
 
 def sort_by_graph_size(stats_table):
     ordered_stats = OrderedDict(sorted(stats_table.items(), key=lambda x:x[1][0]))
@@ -26,13 +28,11 @@ def print_benchmark_stats(stats_table):
 
 def collect_stat(bench_name, stats_table):
     log_file_name = f"{bench_name}.last_executed_graph.dump.log"
-    #print(f"Processing {log_file_name}")
     stat = []
     for grep_str in ops_pattern:
         cmd = f"egrep \"{grep_str}\" {log_file_name} | wc -l"
         ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
         output = ps.communicate()[0]
-        # print(f"{grep_str} occurences: {int(output)}")
         stat.append(int(output))
     stats_table[bench_name] = stat
 
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     # parser.add_argument("json_file")
     # args = parser.parse_args()
     stats_table = {}
+    benchmarks += internal_benchmarks
     for bench_name in benchmarks:
         collect_stat(bench_name, stats_table)
 
