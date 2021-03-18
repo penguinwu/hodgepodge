6459a19f47c17b44c7b8edc376b181adfa26a9b9
Peng Wu <pengwu@fb.com>
2021-03-18 13:42:53 -0700

    Fix test
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- TorchScriptStudy/good-examples/test-tuple.py
+++ TorchScriptStudy/good-examples/test-tuple.py
@@ -3,11 +3,10 @@ import torch
 from typing import Tuple
 from typing import Any
 
-@torch.jit.export
 def incFirstElement(x: Tuple[int, Any]):
      return (x[0]+1, x[1])
 
 m = torch.jit.script(incFirstElement)
-print(incFirstElement((1,2.0)))
-print(incFirstElement((1,(100,200))))
+print(m((1,2.0)))
+print(m((1,(100,200))))
 
